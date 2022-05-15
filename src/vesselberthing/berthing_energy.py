ddecbac88f81afdf6a69db5a3aa02be81352c753
Aaron Fortier <aaron.j.fortier@gmail.com>
2022-05-14 22:46:55 -0700

    Updated to separate E, Plt, & csv functions

--- src/vesselberthing/berthing_energy.py
+++ src/vesselberthing/berthing_energy.py
@@ -3,11 +3,19 @@ plot these energies on fender reaction curves, and generate the resulting
 reactions.
 """
 import csv
+import json
+import os
 
 from .functions import berth_coeff as bc
 from .functions import fndr_curves as fc
 from .functions import fndr_plot as plt
 from .functions import berthvel as bv
+
+const_path = os.path.abspath(os.path.join(os.path.dirname(__file__),'.\\constants'))
+
+f = open(const_path + '\\vessels.json','r')
+vessel_library = json.load(f)
+f.close()
 
 def berthing_energy(vessels, berths, loadcases, fender_dict, cp_e_dict, output):
     """Determine berthing energies using UFC 4-152-01 based on guidance from
@@ -24,25 +32,17 @@ def berthing_energy(vessels, berths, loadcases, fender_dict, cp_e_dict, output):
         Rberth_arr[i] = []
         Dberth_arr[i] = []
 
-    # Field names used for CSV output
-    fldnms = ['Vessel', 'Load Case','Berthing Condition','Acc. Factor', 'Berthing Configuration', 
-            'Water Depth', 'Displacement','Length','Beam','Draft','CoG',
-            'No. Fenders','Fender Name','Fender Type',
-            'Rated Energy', 'Rated Reaction', 'Rated Deflection','Vel', 'Cbl', 
-            'k_r', 'Cg', 'Cd', 'Cc', 'Ce', 'Cb', 'Cm','Eship', 'Deflection', 
-            'Energy', 'Reaction']
-
     for i, key in enumerate(vessels):
         # Assign variables for the current vessel
-        M = vessels[key][0]             # Vessel Displacement
-        L = vessels[key][1]             # Vessel Length
-        B = vessels[key][2]             # Vessel Beam
-        D = vessels[key][3]             # Vessel Draft
-        CG = vessels[key][4]            # Center of Gravity
-        Cbl = bc.Cbl(M, L, B, D)        # Block Factor
-        k_r = bc.k(Cbl, L)              # Radius of Gyration
-        Cd = vessels[key][5]            # Deformation Factor
-        loc_berths = vessels[key][8]
+        M = vessel_library[key]['Displacement']     # Vessel Displacement
+        L = vessel_library[key]['Length Overall']   # Vessel Length
+        B = vessel_library[key]['Breadth']          # Vessel Beam
+        D = vessel_library[key]['Draft']            # Vessel Draft
+        CG = vessel_library[key]['CoG']             # Center of Gravity
+        Cbl = bc.Cbl(M, L, B, D)                    # Block Factor
+        k_r = bc.k(Cbl, L)                          # Radius of Gyration
+        Cd = vessel_library[key]['C_d']             # Deformation Factor
+        loc_berths = vessels[key][2]                # Array of Berths for this vessel
 
         for j, jtem in enumerate(loc_berths):
             loc_fndr_key = (berths[jtem][1])
@@ -71,7 +71,7 @@ def berthing_energy(vessels, berths, loadcases, fender_dict, cp_e_dict, output):
                 # Define Number of Fenders and Eccentricity
                 match config:
                     case 'Broadside':
-                        nfndr = vessels[key][6]
+                        nfndr = vessels[key][0]
                         a = 0
                     case 'Corner Protection':
                         nfndr = 1
@@ -89,7 +89,7 @@ def berthing_energy(vessels, berths, loadcases, fender_dict, cp_e_dict, output):
                 Cb = Ce * Cg * Cd * Cc
 
                 # Virtual Mass Coefficient
-                Cm = bc.Cm(L, B, D, Cb, wdepth, vessels[key][7])
+                Cm = bc.Cm(L, B, D, Cb, wdepth, vessels[key][1])
 
                 # Calculate Fender Energy (includes 10% increase for uncertainty)
                 Efndr = ABF * Cm * Cb * Eship / nfndr * 1.1
@@ -101,15 +101,26 @@ def berthing_energy(vessels, berths, loadcases, fender_dict, cp_e_dict, output):
                     Rberth_arr[loc_fndr_key].append([Rfndr[1]])
                     Dberth_arr[loc_fndr_key].append([Rfndr[0]])
 
-                    results[loc_fndr_key].append([key, k, case, ABF, config, wdepth, 
+                    results[loc_fndr_key].append([key, k, case, ABF, config, a, wdepth, 
                                                 M, L, B, D, CG, nfndr, 
                                                 loc_fndr_key, fender_type, 
                                                 E_rating, R_rating, D_rating, 
                                                 V, Cbl, k_r, Cg, Cd, Cc, Ce, Cb,
                                                 Cm, Eship, Rfndr[0], Efndr, 
                                                 Rfndr[1]])
+    return results
+
+def plot_fndr_results(vessels,fender_dict,fndr_results):
 
     for i in fender_dict.keys():
+        Eberth_arr = []
+        Rberth_arr = []
+        Dberth_arr = []
+
+        for j in fndr_results[i]:
+            Eberth_arr.append(j[29])
+            Rberth_arr.append(j[30])
+            Dberth_arr.append(j[28])
 
         # Generate Fender Curves
         fender_type = fender_dict[i][0]
@@ -119,14 +130,26 @@ def berthing_energy(vessels, berths, loadcases, fender_dict, cp_e_dict, output):
         E_curve = fc.Fenders[fender_type][0]*E_rating
         R_curve = fc.Fenders[fender_type][1]*R_rating
 
-        plt.ER_demand_curve(Dberth_arr[i],Eberth_arr[i],Rberth_arr[i],D_curve,
+        plt.ER_demand_curve(Dberth_arr,Eberth_arr,Rberth_arr,D_curve,
                             E_curve,R_curve,cht_title= i + ' Fender Demand Curves')
         plt.ER_curve(D_curve, E_curve,R_curve, cht_title= i + ' Fender Curves')
 
     data=[]
     for i, key in enumerate(vessels):
-        data.append([key,vessels[key][0],bv.velocity(vessels[key][0],'sheltered')])
+        data.append([key,vessel_library[key]['Displacement'],bv.velocity(vessel_library[key]['Displacement'],'sheltered')])
     plt.vel_plt(data,'Approach Velocity Perpendicular to Berth')
+
+    return
+
+def print_fndr_csv(results,output='Berthing Energy.csv'):
+
+    # Field names used for CSV output
+    fldnms = ['Vessel', 'Load Case','Berthing Condition','Acc. Factor', 'Berthing Configuration', 'Eccentricity',
+            'Water Depth', 'Displacement','Length','Beam','Draft','CoG',
+            'No. Fenders','Fender Name','Fender Type',
+            'Rated Energy', 'Rated Reaction', 'Rated Deflection','Vel', 'Cbl', 
+            'k_r', 'Cg', 'Cd', 'Cc', 'Ce', 'Cb', 'Cm','Eship', 'Deflection', 
+            'Energy', 'Reaction']
 
     f = open(output,'w',newline='')
     writer = csv.writer(f)
